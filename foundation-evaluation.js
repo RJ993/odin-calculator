efdd961e6576db62dee0e1ce4d6531561e6f39dd
RJ993 <196080894+RJ993@users.noreply.github.com>
2025-03-22 16:27:14 -0400

    Make number buttons display numbers

--- foundation-evaluation.js
+++ foundation-evaluation.js
@@ -1,6 +1,17 @@
-const firstNumber = ''
+let firstNumber = ''
 const operators = ['+', '-', 'x', '÷']
-const secondNumber = ''
+let secondNumber = ''
+const one = document.querySelector('#one')
+const two = document.querySelector('#two')
+const three = document.querySelector('#three')
+const four = document.querySelector('#four')
+const five = document.querySelector('#five')
+const six = document.querySelector('#six')
+const seven = document.querySelector('#seven')
+const eight = document.querySelector('#eight')
+const nine = document.querySelector('#nine')
+const zero = document.querySelector('#zero')
+const display = document.querySelector('.display')
 
 function add(a, b){
     return a + b
@@ -29,3 +40,34 @@ function operate(firstNumber, secondNumber){
         divide(firstNumber, secondNumber)
     }
 }
+
+one.addEventListener('click', (e) => {
+    display.append('1')
+});
+two.addEventListener('click', (e) => {
+    display.append('2')
+});
+three.addEventListener('click', (e) => {
+    display.append('3')
+});
+four.addEventListener('click', (e) => {
+    display.append('4')
+});
+five.addEventListener('click', (e) => {
+    display.append('5')
+});
+six.addEventListener('click', (e) => {
+    display.append('6')
+});
+seven.addEventListener('click', (e) => {
+    display.append('7')
+});
+eight.addEventListener('click', (e) => {
+    display.append('8')
+});
+nine.addEventListener('click', (e) => {
+    display.append('9')
+});
+zero.addEventListener('click', (e) => {
+    display.append('0')
+});
